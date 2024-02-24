0c3ce96d8218d77f4c7d11dc64f0ddb123f7f6aa
YOUNESSE25 <younessehaouas@gmail.com>
2024-02-23 17:32:12 -0800

    configuration_management

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,11 @@
-#!/usr/bin/pup
-# install flask from pip3
-package {'flask':
+#puppet declarative to install flask using pip3
+
+package { 'flask':
   ensure   => '2.1.0',
   provider => 'pip3'
+}
+
+package {'Werkzeug':
+  ensure   => '2.1.1',
+  provider => 'pip3',
 }
