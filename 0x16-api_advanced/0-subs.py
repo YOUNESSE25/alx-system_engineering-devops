a872c0a5f77984c1440a7207bf1bc4bae08b86db
YOUNESSE25 <younessehaouas@gmail.com>
2024-05-07 21:25:46 +0100

    api-adv

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,22 +1,17 @@
 #!/usr/bin/python3
-""" function that queries the Reddit API and returns the number
-of subscribers for a given subreddit """
+"""
+Contains the number_of_subscribers function
+"""
+
 import requests
-from sys import argv
 
 
 def number_of_subscribers(subreddit):
-    """ number of subscribers"""
-    if subreddit is None:
+    """returns the number of subscribers for a given subreddit"""
+    if subreddit is None or type(subreddit) is not str:
         return 0
-    if type(subreddit) is not str:
-        return 0
-    hd = {'User-Agent': 'Lizzie'}
-    url = requests.get('https://www.reddit.com/r/{}/about.json'
-                       .format(subreddit), headers=hd).json()
-    sub = url.get("data", {}).get("subscribers", 0)
-    return sub
-
-
-if __name__ == "__main__":
-    number_of_subscribers(argv[1])
+    r = requests.get('http://www.reddit.com/r/{}/about.json'.format(subreddit),
+                     headers={'User-Agent': '0x16-api_advanced:project:\
+v1.0.0 (by /u/firdaus_cartoon_jr)'}).json()
+    subs = r.get("data", {}).get("subscribers", 0)
+    return subs
