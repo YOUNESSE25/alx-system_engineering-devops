bb3e9c128c00c083995668c888456ae0329d1039
YOUNESSE25 <younessehaouas@gmail.com>
2024-05-07 21:18:02 +0100

    api-adv

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 """ function that queries the Reddit API and returns the number
 of subscribers for a given subreddit """
-import requests
 from sys import argv
+import requests
 
 
 def number_of_subscribers(subreddit):
