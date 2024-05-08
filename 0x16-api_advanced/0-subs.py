2e4b0ba71b77aa83296838b7dc83e9212ae62513
YOUNESSE25 <younessehaouas@gmail.com>
2024-05-08 19:51:32 +0100

    api-adv

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -4,22 +4,14 @@
     of subscribers for a given subreddit
 '''
 import requests
-from sys import argv
 
 
 def number_of_subscribers(subreddit):
     '''number of subscribers'''
-    if subreddit is None:
+    if subreddit is None or type(subreddit) is not str:
         return 0
-    if type(subreddit) is not str:
-        return 0
-    user = {"User-Agent": "Custom"}
-    url = requests.get('https://www.reddit.com/r/{}/about.json'
-                       .format(subreddit), headers=user,
-                       allow_redirects=False).json()
-    sub = url.get("data", {}).get("subscribers", 0)
-    return sub
-
-
-if __name__ == "__main__":
-    number_of_subscribers(argv[1])
+    r = requests.get('http://www.reddit.com/r/{}/about.json'.format(subreddit),
+                     headers={'User-Agent': '0x16-api_advanced:project:\
+v1.0.0 (by /u/firdaus_cartoon_jr)'}).json()
+    subs = r.get("data", {}).get("subscribers", 0)
+    return subs
